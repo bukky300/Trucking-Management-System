0a6758322f58c87b1bbceba29c51245774e68f73
bukky300 <bukkybrown11@gmail.com>
2026-02-22 21:50:10 +0100

    small ui change

--- frontend/src/pages/PlanTrip.jsx
+++ frontend/src/pages/PlanTrip.jsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import PageShell from '../components/layout/PageShell'
 import TripPlannerForm from '../components/form/TripPlannerForm'
 import PreviewPanel from '../components/preview/PreviewPanel'
@@ -6,14 +6,22 @@ import PreviewPanel from '../components/preview/PreviewPanel'
 function PlanTrip({ onTripPlanned }) {
   return (
     <PageShell>
-      <Grid container alignItems="stretch">
-        <Grid size={{ xs: 12, md: 5 }}>
-          <TripPlannerForm onResult={onTripPlanned} />
+      <Box
+        sx={{
+          minHeight: { md: 'calc(100vh - 120px)' },
+          display: 'flex',
+          alignItems: { md: 'center' },
+        }}
+      >
+        <Grid container alignItems="stretch" width="100%">
+          <Grid size={{ xs: 12, md: 5 }}>
+            <TripPlannerForm onResult={onTripPlanned} />
+          </Grid>
+          <Grid size={{ xs: 12, md: 7 }}>
+            <PreviewPanel />
+          </Grid>
         </Grid>
-        <Grid size={{ xs: 12, md: 7 }}>
-          <PreviewPanel />
-        </Grid>
-      </Grid>
+      </Box>
     </PageShell>
   )
 }
